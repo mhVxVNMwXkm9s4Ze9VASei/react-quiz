9d42b9b3ead494b0471b7f5aaed4bd9efb085632
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-08-20 23:29:27 -0500

    Few things...
    Added json-server package and loaded questions into the app.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+	questions: [],
+
+	// Valid statuses are active, error, finished, loading and ready.
+	status: "loading",
+};
+
+function reducer(state, action) {
+	switch (action.type) {
+		case "dataFailed":
+			return {
+				...state,
+				status: "error",
+			};
+		case "dataReceived":
+			return {
+				...state,
+				status: "ready",
+				question: action.payload,
+			};
+		default:
+			throw new Error("Unknown action.");
+	}
+}
+
 export default function App() {
+	const [state, dispatch] = useReducer(reducer, initialState);
+	useEffect(() => {
+		fetch("http://localhost:8000/questions")
+			.then((res) => res.json())
+			.then((data) =>
+				dispatch({
+					payload: data,
+					type: "dataReceived",
+				})
+			)
+			.catch((error) => dispatch({ type: "dataFailed" }));
+	}, []);
+
 	return (
 		<div className="app">
 			<Header />
